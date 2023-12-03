17c89aca12022522a7af5c33441531fd1f92468c
kire97 <89543171+kire97@users.noreply.github.com>
2023-12-03 10:17:38 +0100

    Day1 part2 solution

--- day1.py
+++ day1.py
@@ -1,7 +1,26 @@
-with open('day1.input', mode='r') as file:
+def reduce_str(input: str):
+    result = input
+    string_to_int = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, }
+
+    for k, v in string_to_int.items():
+        slices = result.split(k)
+        for slice in slices[:-1]:
+            result += f'{slice}{v}'
+        result += f'{slices[-1]}'
+    return result
+
+def solve(input: list[str]):
     result: int = 0
-    for line in file.readlines():
-        numbers = [c for c in line if c.isnumeric()]
+
+    for line in input:
+
+        numbers = [c for c in reduce_str(line) if c.isnumeric()]
+
         result += int(f'{numbers[0]}{numbers[-1]}')
 
-    print(result)
+
+
+    return result
+    
+with open('day1.input', mode='r') as file:
+    print(solve(file.readlines()))
