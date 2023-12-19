0245283a66f0d074595230be12fc7f7af3de6e61
Erik <e.svensson@tele-radio.com>
2023-12-19 16:59:39 +0100

    solve day4 step2

--- day4.py
+++ day4.py
@@ -1,4 +1,11 @@
 import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Card:
+    matches: int
+    copies: int = 1
 
 
 def looper(a: list[int], b: list[int]) -> int:
@@ -10,16 +17,25 @@ def looper(a: list[int], b: list[int]) -> int:
 
 def solve(in_data: list[str]) -> int:
     result: int = 0
+    result2: int = 0
+    cards: list[Card] = []
     for line in in_data:
-        game_idx = int(line[4:8])
         line_parts = line.split(":")[1].split("|")
         win_nrs = re.findall(r"\d+", line_parts[0])
         our_nrs = re.findall(r"\d+", line_parts[1])
 
-        if matches := looper(win_nrs, our_nrs):
+        matches = looper(win_nrs, our_nrs)
+        if matches:
             result += 2 ** (matches - 1)
 
-    return result
+        cards.append(Card(matches))
+
+    for card_idx, card in enumerate(cards):
+        for offset_idx in range(card.matches):
+            cards[card_idx + offset_idx + 1].copies += card.copies
+        result2 += card.copies
+
+    return result, result2
 
 
 with open("day4.input", "r") as file:
