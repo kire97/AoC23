9c9be7557eef5fb30a0bfbbaaeef3e25e2d30626
Erik <e.svensson@tele-radio.com>
2023-12-19 15:11:05 +0100

    solve day3 part2

--- day3.py
+++ day3.py
@@ -1,38 +1,58 @@
 import re
+from dataclasses import dataclass, field
+from functools import reduce
+
+
+@dataclass
+class SpecialCharacter:
+    x_pos: int
+    y_pos: int
+    character: str
+    neighbor_values: list[int] = field(default_factory=list)
 
 
 def in_range(
-    xy_positions: list[tuple[int, int]],
+    characters: list[SpecialCharacter],
     x_range: tuple[int, int],
     y_range: tuple[int, int],
-) -> bool:
-    for x, y in xy_positions:
+) -> SpecialCharacter | None:
+    for char in characters:
+        x = char.x_pos
+        y = char.y_pos
         if not (x >= x_range[0] and x <= x_range[1]):
             continue
         if not (y >= y_range[0] and y <= y_range[1]):
             continue
-        return True
+        return char
     else:
-        return False
+        return None
 
 
 def solve(in_data: list[str]) -> int:
     result: int = 0
-    special_positions: list[tuple[int, int]] = []
+    result2: int = 0
+    special_characters: list[SpecialCharacter] = []
     for row_idx, line in enumerate(in_data):
         for col_idx, char in enumerate(line.strip()):
             if not char.isnumeric() and char != ".":
-                special_positions.append((row_idx, col_idx))
+                special_characters.append(
+                    SpecialCharacter(col_idx, row_idx, char)
+                )
 
     for row_idx, line in enumerate(in_data):
         for match in re.finditer(r"\d+", line):
             value = int(match.group(0))
             col_range = (match.start(0) - 1, match.end(0))
             row_range = (row_idx - 1, row_idx + 1)
-            if in_range(special_positions, row_range, col_range):
+            if char := in_range(special_characters, col_range, row_range):
+                char.neighbor_values.append(value)
                 result += value
 
-    return result
+    for char in special_characters:
+        if char.character == "*" and len(char.neighbor_values) > 1:
+            result2 += reduce(lambda a, b: a * b, char.neighbor_values)
+
+    return result, result2
 
 
 with open("day3.input", "r") as file:
