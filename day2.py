17aa3a0dbb21d076a6ca3b4969e6c27fe7834d3a
Erik <e.svensson@tele-radio.com>
2023-12-19 12:14:51 +0100

    day2 part1 solution

---
+++ day2.py
@@ -0,0 +1,31 @@
+import re
+from functools import reduce, partial
+
+
+def solve(in_data: list[str]) -> int:
+    result: int = 0
+    color_to_max = {"red": 12, "green": 13, "blue": 14}
+
+    for line in in_data:
+        game_idx = int(re.findall(r"(?<=Game )\d+", line)[0])
+
+        def reducer(last: bool, value: int, max_value: int):
+            return last and value <= max_value
+
+        passed = True
+        for color, value in color_to_max.items():
+            rolls = re.findall(rf"\d+(?= {color})", line)
+            rolls = [int(x) for x in rolls]
+            passed = reduce(
+                partial(reducer, max_value=value),
+                rolls,
+                passed,
+            )
+        if passed:
+            result += game_idx
+
+    return result
+
+
+with open("day2.input", "r") as file:
+    print(solve(file.readlines()))
