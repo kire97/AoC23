53006b6c5907a46b7646d429713c47339c5ce736
kire97 <erik.svensson97@hotmail.se>
2023-12-24 23:50:21 +0100

    add day9 step2 solution

--- day9.py
+++ day9.py
@@ -11,6 +11,7 @@ def extrapolate(in_data: list[int]) -> list[int]:
 
 def solve(in_data: list[str]) -> int:
     result: int = 0
+    result2: int = 0
     for line in in_data:
         values = [int(v) for v in line.split()]
         lists: list[list[int]] = []
@@ -19,12 +20,13 @@ def solve(in_data: list[str]) -> int:
             values = extrapolate(values)
 
         result += reduce(lambda a, b: a + b[-1], lists, 0)
+        result2 += reduce(lambda a, b: b[0] - a, reversed(lists), 0)
 
-    return result
+    return result, result2
 
 
 with open("day9.test_input", "r") as file:
-    assert solve(file.readlines()) == 114
+    assert solve(file.readlines()) == (114, 2)
 
 with open("day9.input", "r") as file:
     print(solve(file.readlines()))
