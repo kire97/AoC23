c899c2b9731146f85cc61570f8e0e345a7821164
kire97 <erik.svensson97@hotmail.se>
2023-12-24 23:44:18 +0100

    add day9 part1 solution

---
+++ day9.py
@@ -0,0 +1,30 @@
+from functools import reduce
+
+
+def extrapolate(in_data: list[int]) -> list[int]:
+    out_data: list[int] = []
+    for idx in range(len(in_data) - 1):
+        out_data.append(in_data[idx + 1] - in_data[idx])
+
+    return out_data
+
+
+def solve(in_data: list[str]) -> int:
+    result: int = 0
+    for line in in_data:
+        values = [int(v) for v in line.split()]
+        lists: list[list[int]] = []
+        while [x for x in values if x != 0]:
+            lists.append(values)
+            values = extrapolate(values)
+
+        result += reduce(lambda a, b: a + b[-1], lists, 0)
+
+    return result
+
+
+with open("day9.test_input", "r") as file:
+    assert solve(file.readlines()) == 114
+
+with open("day9.input", "r") as file:
+    print(solve(file.readlines()))
